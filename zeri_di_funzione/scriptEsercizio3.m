98a5d995f232fc26f926887056947192d7f4c688
Chiara Margari <100866036+ChiaraMargari@users.noreply.github.com>
2023-05-12 17:30:04 +0200

    Delete zeri_di_funzione directory

--- zeri_di_funzione/scriptEsercizio3.m
+++
@@ -1,8 +0,0 @@
-f=@(x) 1 - x^2 / 2 - x^3;
-d1=@(x) -x - 3*(x)^2;
-tol= 1e-10;
-x0= .99;
-maxiter= 100;
-[xk, iter] = esercizio3(f, d1, x0, tol, maxiter);
-fprintf("Risultato: %f\n", xk);
-fprintf("Numero iterazioni: %d\n", iter);
--- zeri_di_funzione/scriptEsercizio4.m
+++
@@ -1,7 +0,0 @@
-S=286797;
-n=5;
-tol= 1e-10;
-maxiter=100;
-[xk, iter] = esercizio4(S, n, tol, maxiter);
-fprintf("Risultato: %f\n", xk);
-fprintf("Numero di iterate: %d", iter);
