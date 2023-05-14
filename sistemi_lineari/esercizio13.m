4d382fd69a7457a4e6e3c0ef21eecc0380c9761b
Chiara Margari <100866036+ChiaraMargari@users.noreply.github.com>
2023-05-14 22:22:48 +0200

    Update esercizio13.m

--- sistemi_lineari/esercizio13.m
+++ sistemi_lineari/esercizio13.m
@@ -4,14 +4,14 @@ function[xi] = esercizio13(A, b)
 %inferiore
 
 n = size(A, 1);
-xi = zeros(n, 1);
-xi(1) = b(1) /  A(1,1);
+x = zeros(n, 1);
+x(1) = b(1) /  A(1,1);
 
 for i = 2 : n
     somma = 0;
     for j = 1 : i-1
-    somma = somma + A(i, j) * xi(j);
+    somma = somma + A(i, j) * x(j);
     end
 
-    xi(i) = (1 / A(i , 1)) * (b(i) - somma);
+    x(i) = (1 / A(i , 1)) * (b(i) - somma);
 end
